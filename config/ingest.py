f7712576be6e0ec6b5bfc15ec98a0ee2dcfaf121
Michael Wood-Vasey <wmwv@pitt.edu>
2018-03-02 10:31:38 -0500

    Add config for ingest.py

---
+++ config/ingest.py
@@ -0,0 +1,30 @@
+from lsst.obs.wiyn.ingest import WhircParseTask
+
+config.parse.retarget(WhircParseTask)
+
+config.parse.translation = {
+    'expTime': 'EXPTIME',
+    'object': 'OBJECT',
+    'imageType': 'IMGTYPE',
+    'filter': 'FILTER1',
+    'date': 'DATE-OBS',
+    'dateObs': 'DATE-OBS',
+    'imageType': 'IMGTYPE',
+}
+config.parse.translators = {
+    'visit': 'translate_visit',
+}
+config.register.columns = {
+    'visit': 'int',
+    'basename': 'text',
+    'filter': 'text',
+    'date': 'text',
+    'dateObs': 'text',
+    'expTime': 'double',
+    'expnum': 'int',
+    'ccd': 'int',
+    'object': 'text',
+    'imageType': 'text',
+}
+config.register.visit = list(config.register.columns.keys())
+config.register.unique = ['visit']
