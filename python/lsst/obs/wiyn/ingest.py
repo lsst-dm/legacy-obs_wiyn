b27d347e26bf4e504ee69f2a9a1b1f8483064d33
Michael Wood-Vasey <wmwv@pitt.edu>
2018-03-06 05:49:14 -0800

    Add ingestStack config.

--- python/lsst/obs/wiyn/ingest.py
+++ python/lsst/obs/wiyn/ingest.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 from datetime import (datetime, timedelta)
 import os
+import re
 
 from lsst.pipe.tasks.ingest import ParseTask
 
@@ -57,3 +58,62 @@ class WhircParseTask(ParseTask):
         # relative to 2010-01-01, just to make the visits ints a bit smaller
         mmjd = mjd - 55197
         return int(1e5*mmjd)  # 86400s per day, so we need this resolution
+
+
+class WhircStackParseTask(ParseTask):
+    """Parser suitable for stacked data
+
+    Stacke data are from a sequence of dithered observations
+    that have been ISRed and stacked together.
+    """
+
+    def getInfo(self, filename):
+        # Grab the basename
+        phuInfo, infoList = ParseTask.getInfo(self, filename)
+        basename = os.path.basename(filename)
+        while any(basename.endswith("." + ext) for ext in EXTENSIONS):
+            basename = basename[:basename.rfind('.')]
+        phuInfo['basename'] = basename
+        # Extract 'seq' from filename.  It's not stored in header.
+        basenameRegex = "([^_]+)_([A-Z])_(J|H|KS)_([0-9]{8,})"
+        field, seq, filt, night = re.match(basenameRegex, basename).groups()
+        phuInfo['seq'] = seq
+        phuInfo['night'] = int(night)
+        return phuInfo, infoList
+
+    def translate_visit(self, md):
+        """Generate a unique visit from the timestamp.
+
+        It might be better to use the 1000*runNo + seqNo,
+        but the latter isn't currently set
+
+        Parameters
+        ----------
+        md : `lsst.daf.base.PropertyList or PropertySet`
+            image metadata
+
+        Returns
+        -------
+        visit_num : `int`
+            Visit number, as translated
+        """
+        mjd = md.get("MJD-OBS")
+        # relative to 2010-01-01, just to make the visits ints a bit smaller
+        mmjd = mjd - 55197
+        return int(1e5*mmjd)  # 86400s per day, so we need this resolution
+
+    def getDestination(self, butler, info, filename):
+        """Get destination for the file
+
+        @param butler      Data butler
+        @param info        File properties, used as dataId for the butler
+        @param filename    Input filename
+        @return Destination filename
+        """
+        dataset = 'stack'
+        destination = butler.get("%s_filename" % dataset, info)[0]
+        # Ensure filename is devoid of cfitsio directions about HDUs
+        c = destination.find("[")
+        if c > 0:
+            destination = destination[:c]
+        return destination
