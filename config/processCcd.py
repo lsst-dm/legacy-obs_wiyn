5186217893ccf7ba1596133ba2abe1d6254d69b8
Michael Wood-Vasey <wmwv@pitt.edu>
2018-03-06 16:57:20 -0800

    Trying to figure out how to get retarget to WhircIsrTask.
    I think I have things set up, but it's still calling IsrTask.

--- config/processCcd.py
+++ config/processCcd.py
@@ -1,13 +1,8 @@
 # Don't do ISR right now
-# from lsst.obs.wiyn.whirc.isr import WhircIsrTask
-# calib.isr.retarget(WhircIsrTask)
+from lsst.obs.wiyn.isr import WhircIsrTask
+config.isr.retarget(WhircIsrTask)
 
-# calib.isr.doBias = False
-# calib.isr.doDark = False
-# calib.isr.doFlat = False
-# calib.isr.doWrite = False
-# calib.isr.assembleCcd.setGain = False
-
-# Focus on post-ISR, post-dither-mosaicing images:
-calib.calibrate.repair.doCosmicRay = False # Currently troublesome: lots of pixels that need to be masked first
-calib.calibrate.repair.cosmicray.nCrPixelMax = 100000
+config.isr.doBias = False
+config.isr.doDark = False
+config.isr.doFlat = False
+config.isr.doWrite = False
