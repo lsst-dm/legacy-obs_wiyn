7e11cb255eb894e8d70ae0dc98e79c2cbdcdf0f2
Michael Wood-Vasey <wmwv@pitt.edu>
2018-03-07 22:17:46 -0800

    Add ingestStack.py config.

---
+++ config/ingestStack.py
@@ -0,0 +1,30 @@
+from lsst.obs.wiyn.ingest import WhircStackParseTask
+
+config.parse.retarget(WhircStackParseTask)
+
+config.parse.translation = {
+    'expTime': 'EXPTIME',
+    'imageType': 'IMGTYPE',
+    'filter': 'FILTER1',
+    'date': 'DATE-OBS',
+    'dateObs': 'DATE-OBS',
+    'imageType': 'IMGTYPE',
+}
+config.parse.translators = {
+    'visit': 'translate_visit',
+}
+config.register.table = 'stack'
+config.register.columns = {
+    'visit': 'int',
+    'basename': 'text',
+    'field': 'text',
+    'seq': 'text',
+    'filter': 'text',
+    'night': 'int',
+    'date': 'text',
+    'dateObs': 'text',
+    'expTime': 'double',
+    'imageType': 'text',
+}
+config.register.visit = list(config.register.columns.keys())
+config.register.unique = ['field', 'seq', 'filter', 'night']
