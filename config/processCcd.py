48216a8d972c53ab900238792a8c2b46819a7637
Michael Wood-Vasey <wmwv@pitt.edu>
2018-03-15 16:49:35 -0400

    Try MatchPessimisticBTask for astrom matching.

--- config/processCcd.py
+++ config/processCcd.py
@@ -1,3 +1,5 @@
 # Don't do ISR right now
 from lsst.obs.wiyn.isr import WhircNullIsrTask
 config.isr.retarget(WhircNullIsrTask)
+from lsst.meas.astrom.matchPessimisticB import MatchPessimisticBTask
+config.calibrate.astrometry.matcher.retarget(MatchPessimisticBTask)
